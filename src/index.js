da9de99861fa59c9118b3c4a138f8a1c98dc620e
salim-ingram <98127818+salim-ingram@users.noreply.github.com>
2022-12-28 12:13:40 -0800

    ,

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import CollectionContainer from "./components/CollectionContainer"
 
-ReactDOM.render(<CollectionContainer />, document.getElementById("root"))
+ReactDOM.render(
+    <React.StrictMode>
+        <CollectionContainer />
+    </React.StrictMode>,
+    document.getElementById("root"))
